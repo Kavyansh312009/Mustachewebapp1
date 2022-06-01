76112f229d4765961d6ddb9b4dfc134ae9941bbb
Kavyansh312009 <94540332+Kavyansh312009@users.noreply.github.com>
2022-06-01 17:46:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    img = createCapture(VIDEO);
+    img.hide();
+}
+function draw(){
+    image(img,0,0,300,300);
+}
+function take_snapshot(){
+    save('realfilter.png')
+}
